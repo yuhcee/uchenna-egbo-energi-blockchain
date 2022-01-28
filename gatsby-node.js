0022ef38228f39049c582e58b88e4776ddffde9d
YuhCee <yuhcee@YuhCees-MacBook-Pro.local>
2022-01-28 14:50:01 +0100

    fix webpack issues

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const webpack = require("webpack");
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
@@ -5,5 +7,27 @@ exports.createPages = async ({ actions }) => {
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
+  })
+}
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+      plugins: [
+          new webpack.ProvidePlugin({
+              Buffer: [require.resolve("buffer/"), "Buffer"],
+              process: 'process/browser'
+          }),
+      ],
+      resolve: {
+          fallback: {
+              "crypto": false,
+              "stream": require.resolve("stream-browserify"),
+              "assert": false,
+              "util": false,
+              "http": false,
+              "https": false,
+              "os": false
+          },
+      },
   })
 }
